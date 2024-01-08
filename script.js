5562b78e6d23527f8b93f210884ea73f1a1c590e
Ant Buffill <abuffill@outlook.com>
2024-01-07 23:10:56 -0600

    updated code after testing viablitity

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 $(function () {
-  // Function to display the current day
+ 
   function displayCurrentDay() {
+    
     let currentDate = dayjs().format('dddd, MMMM D');
+    
     console.log('Current Hour:', dayjs().hour());
     $('#currentDay').text(currentDate);
   }
 
-  // Function to update time block colors based on current time
+  
   function updateTimeBlocks() {
     const currentHour = dayjs().hour();
   
     $('.time-block').each(function () {
-      // Extract blockHour from the id attribute
+      
       let blockHour = parseInt($(this).attr('id').replace('hour-', ''), 10);
   
       console.log('Block Hour:', blockHour);
@@ -19,43 +21,47 @@ $(function () {
   
       if (blockHour < currentHour) {
         $(this).removeClass('present').addClass('past');
-      } else if (blockHour === currentHour) {
+      } 
+       else if (blockHour === currentHour) {
         $(this).removeClass('future').addClass('present');
-      } else {
+      }
+       else {
         $(this).removeClass('present').addClass('future');
       }
     });
   }
 
-  // Function to save events to local storage
-  function saveEvent() {
-    var eventId = $(this).parent().attr('id');
-    var eventText = $(this).siblings('.description').val();
+  
+  function storeEntry() {
+    
+    let entryId = $(this).parent().attr('id');
+    let entryText = $(this).siblings('.description').val();
 
-    localStorage.setItem(eventId, eventText);
+    localStorage.setItem(entryId, entryText);
   }
 
-  // Function to load events from local storage
-  function loadEvents() {
+  
+  function loadEntrys() {
     $('.time-block').each(function () {
-      var eventId = $(this).attr('id');
-      var savedEvent = localStorage.getItem(eventId);
+     
+      let storedId = $(this).attr('id');
+      let savedEntry = localStorage.getItem(storedId);
 
-      if (savedEvent) {
-        $(this).find('.description').val(savedEvent);
+      if (savedEntry) {
+        $(this).find('.description').val(savedEntry);
       }
     });
   }
 
-  // Event listener for save button
-  $('.saveBtn').on('click', saveEvent);
+  
+  $('.saveBtn').on('click', storeEntry);
 
-  // Initial setup
+  
   displayCurrentDay();
   updateTimeBlocks();
-  loadEvents();
+  loadEntrys();
 
-  // Set interval to update time blocks every minute
+  
   setInterval(function () {
     updateTimeBlocks();
   }, 60000);
